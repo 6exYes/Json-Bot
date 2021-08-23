fe870cde7e249d4ace1b0e585727cf1a5e65afc1
DJ <87504262+6exYes@users.noreply.github.com>
2021-08-23 15:10:59 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+client.queue = new Map()
+client.config = {
+  prefix: process.env.PREFIX
+}
+
+fs.readdir(__dirname + "/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    console.log("Loading Event: "+eventName)
+  });
+});
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+    console.log("Loading Command: "+commandName)
+  });
+});
+
+client.login(process.env.TOKEN)
